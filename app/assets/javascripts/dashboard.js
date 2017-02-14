cadb43aae3a198b7c7c665b7c8bc247b237e2974
lucas <lucas64_64@hotmail.com>
2017-02-13 21:35:06 -0300

    Adding some template files

---
+++ app/assets/javascripts/dashboard.js
@@ -0,0 +1,30 @@
+//= require jquery/dist/jquery.min.js
+//= require bootstrap/dist/js/bootstrap.min
+//= require fastclick/lib/fastclick.js
+//= require nprogress/nprogress.js
+//= require Chart.js/dist/Chart.min.js
+//= require gauge.js/dist/gauge.min.js
+//= require iCheck/icheck.min.js
+//= require skycons/skycons.js
+
+//= require Flot/jquery.flot.js
+//= require Flot/jquery.flot.pie.js
+//= require Flot/jquery.flot.time.js
+//= require Flot/jquery.flot.stack.js
+//= require Flot/jquery.flot.resize.js
+
+//= require flot.orderbars/js/jquery.flot.orderBars.js
+//= require flot-spline/js/jquery.flot.spline.min.js
+//= require flot.curvedlines/curvedLines.js
+
+//= require DateJS/build/date.js
+
+//= require jqvmap/dist/jquery.vmap.js
+//= require jqvmap/dist/maps/jquery.vmap.world.js
+//= require jqvmap/examples/js/jquery.vmap.sampledata.js
+
+//= require moment/min/moment.min.js
+//= require bootstrap-daterangepicker/daterangepicker.js
+
+
+//= require custom.min.js
