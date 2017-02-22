5dfc58427bc212ef8752ad9596189bcb212c1889
lucas <lucas64_64@hotmail.com>
2017-02-22 01:24:27 -0300

    Criado advertisement e link do logout passado para o template

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170211021241) do
+ActiveRecord::Schema.define(version: 20170222040625) do
+
+  create_table "advertisements", force: :cascade do |t|
+    t.date     "date"
+    t.string   "reason"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "members", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
